89e239c771ded1bf8bb4c4bba633b6656c873316
Wouter van Koppen <wouter.van.koppen@priva.com>
2021-03-07 19:51:01 +0100

    Add user service

---
+++ src/app/user.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './state/user.state';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getUser(): Observable<User> {
+    return this.httpClient.get<User>('assets/user.json');
+  }
+}
